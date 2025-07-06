4588456b85c51ab8758992e8f3045a25c8345cbe
gufronnakaaw <gufronnakaaw@gmail.com>
2025-07-06 18:47:24 +0700

    chore: delete trailingSlash config

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   devIndicators: false,
-  trailingSlash: true,
   experimental: {
     optimizePackageImports: ["@phosphor-icons/react", "react-hot-toast"],
   },
